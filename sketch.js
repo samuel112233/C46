615ca2b6a74c012b4748822ff16fcd2f26bea491
samuel112233 <samuelbhowmik@gmail.com>
2020-06-18 13:01:55 +0530

    stage2

--- sketch.js
+++ sketch.js
@@ -10,11 +10,19 @@ var boy_img;
 var ground;
 var virus,virus_img;
 
+var obstacles,obstacles_img;
+
+var message1,message1_img;
+
+var life = 10;
+
 //to load the images
 function preload(){
-  bg = loadImage("images/bg.jpg");
+  bg = loadImage("images/bggarden01.jpg");
   boy_img = loadAnimation("images/boy1.png","images/boy2.png","images/boy3.png","images/boy4.png","images/boy5.png","images/boy6.png","images/boy7.png","images/boy8.png","images/boy9.png","images/boy10.png");
-  virus_img = loadImage("images/virus.png")
+  virus_img = loadImage("images/virus.png");
+  obstacles_img = loadAnimation("images/rw1.png","images/rw2.png","images/rw3.png","images/rw4.png","images/rw5.png","images/rw6.png","images/rw7.png","images/rw8.png","images/rw9.png");
+  message1_img = loadImage("images/SocialDistancingred.png");
 }
 
 function setup() {
@@ -24,7 +32,9 @@ function setup() {
   world = engine.world;
   player = createSprite(100,300,50,50);
   player.addAnimation("player",boy_img)
-  player.scale=0.6;
+  player.scale=0.4;
+  player.debug = true;
+  player.setCollider("circle",0,0,20,20)
 
   ground = createSprite(600,390,1200,20);
   ground.visible = false;
@@ -33,6 +43,17 @@ function setup() {
   virus.addImage("virus",virus_img);
   deltaX = 0;
   deltaY = 0;
+
+  obstacles = createSprite(1200,300,40,40);
+  obstacles.addAnimation("man1",obstacles_img);
+  obstacles.scale = 0.9;
+  obstacles.velocityX = -4;
+  obstacles.debug = true;
+
+  message1  = createSprite(1000,100,20,20);
+  message1.addImage("m1",message1_img);
+  message1.scale = 0.4;
+  message1.visible = false;
 }
 
 function draw() {
@@ -40,7 +61,7 @@ function draw() {
   Engine.update(engine) 
 
   if(keyDown("space")){
-    player.velocityY=-4;
+    player.velocityY=-8;
 }
   player.velocityY=player.velocityY+1;
   player.collide(ground);
@@ -52,6 +73,29 @@ function draw() {
     player.velocityX=0;
   }
   
+  if(keyCode === 32 && player.y>320){
+    player.velocityY = -3;
+    message1.visible=true;
+  }
+  player.velocityY = player.velocityY+0.5;
+
+   if(obstacles.x<0){
+     obstacles.x = 1200;
+   }
+
+//collision detection betweeen boy and obstacles
+if(player.x-obstacles.x === player.width/2+obstacles.width/2){
+  console.log("working")
+ life = life-1;
+  player.visible=false;
+}
+
+text ("LIFE"+life,50,100);
+
+
+
+
+
   drawSprites();
 }
 
@@ -66,6 +110,14 @@ var followerAngle = Math.atan(deltaY/deltaX);
  follower.velocityX = velocity*cos(followerAngle)
  follower.velocityX = velocity*sin(followerAngle)
 }
+
+//function spawnObstacles(){
+  //if(World.frameCount % 50 === 0){
+
+  
+  //}
+
+//}
 
 
 
